68963e67fa7f252d96b33fd170553a6f2ed2824c
Hossam fawzy <hfawzii99@gmail.com>
2026-08-29 00:34:08 +0300

    game

--- NTI/Tasks/task1/work/one_hot_tb/_primary.vhd
+++ NTI/Tasks/task1/work/one_hot_tb/_primary.vhd
@@ -1,9 +1,4 @@
 library verilog;
 use verilog.vl_types.all;
 entity one_hot_tb is
-    port(
-        clk             : in     vl_logic;
-        rst             : in     vl_logic;
-        led             : out    vl_logic_vector(9 downto 0)
-    );
 end one_hot_tb;
